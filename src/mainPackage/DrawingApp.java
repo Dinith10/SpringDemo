a873ce5ead2e131d2552ed8b41eae4a62187ed05
Bucky Roberts <dinithkalpage@gmail.com>
2019-06-04 20:35:40 +0530

    create beans

--- src/mainPackage/DrawingApp.java
+++ src/mainPackage/DrawingApp.java
@@ -2,18 +2,23 @@ package mainPackage;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class DrawingApp {
 
     public static void main(String[] args) {
 
-    //Triangle triangle = new Triangle();  triangle.draw();
+    //  Triangle triangle = new Triangle();  triangle.draw();
 
-        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+      //  BeanFactory factory = new XmlBeanFactory(new FileSystemResource("C:\\Users\\Harshani Himali\\Desktop\\Spring Boot Work Space\\SpringDemo\\src\\spring.xml"));
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
 
-        factory.getBean("triangle");
+       Triangle triangle = (Triangle) context.getBean("triangle6");
+       triangle.draw();
 
 
 
--- src/mainPackage/Triangle.java
+++ src/mainPackage/Triangle.java
@@ -2,9 +2,47 @@ package mainPackage;
 
 public class Triangle {
 
+
+
+    private String type;
+    private int height;
+
+
+
+    public Triangle() {
+    }
+
+    public Triangle(String type) {
+        this.type = type;
+    }
+
+    public Triangle(String type, int height) {
+        this.type = type;
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public String getType() {
+
+        return type;
+    }
+
+    public Triangle(int height) {
+        this.height = height;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     public  void draw(){
 
         System.out.println("Triangle drawn");
+        System.out.println("Type = "+getType());
+        System.out.println("Height = "+ getHeight());
 
 
 
